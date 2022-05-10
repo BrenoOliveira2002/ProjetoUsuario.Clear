31aef36576e2d1eac7a86dbacadb72c08105d17a
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-10 11:09:48 -0300

    starting using json

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,18 +1,23 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 fields.forEach(function(fields, index, ){
 
     if (fields.name == "gender") {
         if (fields.checked) {
 
             console.log("Sim", fields);
+            user[fields.name] = fields.value;
 
  }
     }
     else {
 
         console.log("Não");
+        user[fields.name] = fields.value;
     }
 
     //console.log(fields);
 
 });
+
+console.log(user);
