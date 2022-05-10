a0d86d425a20032268b6bde8cf0b1584845ffdc6
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-10 10:57:45 -0300

    using query selectors to check markup

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,8 +1,18 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelector("#form-user-create [name = gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var fields = document.querySelectorAll("#form-user-create [name]");
+fields.forEach(function(fields, index, ){
+
+    if (fields.name == "gender") {
+        if (fields.checked) {
+
+            console.log("Sim", fields);
+
+ }
+    }
+    else {
+
+        console.log("Não");
+    }
+
+    //console.log(fields);
+
+});
