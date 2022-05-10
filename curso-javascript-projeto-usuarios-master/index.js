f494d9694a09952710170c70f7cd9c8893c4b262
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-10 12:36:50 -0300

    manipulating html using innerHTML

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -2,31 +2,47 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(dataUser) {
+
+    var tr = document.createElement("tr");
+
+    tr.innerHTML = `
+     <tr>
+     <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+     <td>${dataUser.name}</td>
+     <td>${dataUser.email}</td>
+     <td>${dataUser.admin}</td>
+     <td>${dataUser.birth}</td>
+     <td>
+          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  </tr>
+  `;
+
+  document.getElementById("table-users").appendChild(tr);
+}
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
 
-    alert("Oi!");
     event.preventDefault();
+
     fields.forEach(function(fields, index, ){
 
         if (fields.name == "gender") {
             if (fields.checked) {
     
-                console.log("Sim", fields);
                 user[fields.name] = fields.value;
     
      }
         }
         else {
     
-            console.log("Não");
             user[fields.name] = fields.value;
         }
     
-        //console.log(fields);
-    
     });
 
-});
+   addLine(user);
 
-console.log(user);
+});
