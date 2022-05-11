14155f731342caa98b956eb3d12b77e5c598f44a
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-11 12:50:01 -0300

    Creating a static class for date

--- curso-javascript-projeto-usuarios-master/controllers/userControllers.js
+++ curso-javascript-projeto-usuarios-master/controllers/userControllers.js
@@ -13,6 +13,10 @@ class UserController {
 
             event.preventDefault();
 
+            let bnt = this.formEl.querySelector("[type=submit]")
+
+            bnt.disabled = true; 
+
             let values = this.getValues();
 
             this.getPhotos().then(
@@ -21,6 +25,10 @@ class UserController {
                 values.photo = content;
 
                 this.addLine(values);
+
+                this.formEl.reset();
+
+                bnt.disabled = false;
                 
             },
             (e)=>{
@@ -116,7 +124,7 @@ class UserController {
          <td>${dataUser.name}</td>
          <td>${dataUser.email}</td>
          <td>${(dataUser.admin) ? 'Sim' : "Não"}</td>
-         <td>${dataUser.birth}</td>
+         <td>${dataUser.register}</td>
          <td>
               <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
               <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
